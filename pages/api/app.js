6dd89da2f125b4d25e2ddd2f8f849536eee9f7e7
raagavi2105 <raagavi2105@users.noreply.github.com>
2025-09-30 11:52:25 +0530

    fix: Next build for Vercel (API TS types, app/ shims)

--- pages/api/app.js
+++ pages/api/app.js
@@ -1,12 +1,3 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from '../../src/store';
-import { persistStore } from 'redux-persist';
-
-const store = createStore(rootReducer);
-const persistor = persistStore(store);
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'API is working', store, persistor });
+export default function handler(req, res) {
+  res.status(200).json({ message: 'API is working' });
 }
